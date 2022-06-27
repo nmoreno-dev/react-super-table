c0e7622046fe4a4bfd117ead032045596c461a5e
Nahuel1705 <nahuelmoreno1705@gmail.com>
2022-06-27 18:56:09 -0300

    Implemented TableTitle component with styles

--- src/components/UpperControlBar/UperControlBar.tsx
+++ src/components/UpperControlBar/UperControlBar.tsx
@@ -1,46 +1,59 @@
-import React from "react"
-import { ControlBarContainer, PagesAndSearchControlBarContainer } from "./UpperControlBar.elements"
+import React from "react";
+import {
+  ControlBarContainer,
+  PagesAndSearchControlBarContainer,
+  TableTitle,
+} from "./UpperControlBar.elements";
 
 type UpperControlBarProps = {
-    title?: string,
-    rowsAmount: number,
-    rowLength: number,
-    showSearcher?: boolean,
-    handleRowsAmmountToShow: (event: React.ChangeEvent<HTMLSelectElement>) => void,
-    handleSearchInputChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
-}
+  title?: string;
+  rowsAmount: number;
+  rowLength: number;
+  showSearcher?: boolean;
+  handleRowsAmmountToShow: (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => void;
+  handleSearchInputChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
 export const UpperControlBar = (props: UpperControlBarProps) => {
+  const {
+    title,
+    rowsAmount,
+    rowLength,
+    showSearcher,
+    handleRowsAmmountToShow,
+    handleSearchInputChange,
+  } = props;
 
-    const {
-        title,
-        rowsAmount,
-        rowLength,
-        showSearcher,
-        handleRowsAmmountToShow,
-        handleSearchInputChange
-    } = props
-
-    return(
-        <ControlBarContainer>
-        {title && <h3>{title}</h3>}
-            <PagesAndSearchControlBarContainer>
+  return (
+    <ControlBarContainer>
+      {title && <TableTitle>{title}</TableTitle>}
+      <PagesAndSearchControlBarContainer>
         <span>
-        Showing{' '}
-        <select name="rowsPerPage" id="rowsPerPage" onChange={handleRowsAmmountToShow}>
+          Showing{" "}
+          <select
+            name="rowsPerPage"
+            id="rowsPerPage"
+            onChange={handleRowsAmmountToShow}
+          >
             {rowsAmount >= 10 && <option value={10}>10</option>}
             {rowsAmount >= 25 && <option value={25}>25</option>}
             {rowsAmount >= 50 && <option value={50}>50</option>}
             {rowsAmount >= 100 && <option value={100}>100</option>}
             <option value={rowLength}>All</option>
-        </select>{' '}
-        of {rowsAmount} entries.
+          </select>{" "}
+          of {rowsAmount} entries.
         </span>
         {showSearcher && (
-        <span>
-            Search: <input placeholder="Start typing here..." onChange={handleSearchInputChange} />
-        </span>
+          <span>
+            Search:{" "}
+            <input
+              placeholder="Start typing here..."
+              onChange={handleSearchInputChange}
+            />
+          </span>
         )}
-    </PagesAndSearchControlBarContainer>
-        </ControlBarContainer>
-  )
-}
+      </PagesAndSearchControlBarContainer>
+    </ControlBarContainer>
+  );
+};
