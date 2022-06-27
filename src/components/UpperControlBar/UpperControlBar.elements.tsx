3198fc64f048a0a7d48ac504a94a1cc94072b3fc
Nahuel1705 <nahuelmoreno1705@gmail.com>
2022-06-27 18:54:55 -0300

    Created TableTitle component

--- src/components/UpperControlBar/UpperControlBar.elements.tsx
+++ src/components/UpperControlBar/UpperControlBar.elements.tsx
@@ -1,18 +1,15 @@
-import styled from 'styled-components'
-
+import styled from "styled-components";
 
 export const ControlBarContainer = styled.div`
-    background-color: hsl(210, 29%, 29%);
-    color: hsl(210, 9%, 92%);
-    text-align: left;
-    h3{
-        margin: 0%;
-    }
-    padding: 10px;
-`
-
+  background-color: hsl(210, 29%, 29%);
+  color: hsl(210, 9%, 92%);
+  text-align: left;
+  padding: 10px;
+`;
+export const TableTitle = styled.h2`
+  margin: 0% 0% 5px 0%;
+`;
 export const PagesAndSearchControlBarContainer = styled.div`
-    display: flex;
-    font-size: 1.1rem;
-    justify-content: space-between
-`
+  display: flex;
+  justify-content: space-between;
+`;
