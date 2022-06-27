e38dd61ebed6c68e456884f9dc9d4fc306654c00
Nahuel1705 <nahuelmoreno1705@gmail.com>
2022-06-27 19:31:11 -0300

    Extending ButtonProps from button element

--- src/types/Button.types.ts
+++ src/types/Button.types.ts
@@ -1,13 +1,9 @@
 import React, { MouseEventHandler, ReactElement } from "react";
 
-export type ButtonProps = {
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   label?: string;
-  onclick: MouseEventHandler;
   separator?: boolean;
   circle?: boolean;
   icon?: JSX.Element;
-  margin?: string;
-  fontSize?: string;
-  disabled?: boolean;
-  style?: React.CSSProperties;
-};
+}
