0483374d59a91922887a2d601c830671492bf14c
Nahuel1705 <nahuelmoreno1705@gmail.com>
2022-06-26 22:28:51 -0300

    export styled componetn for UpperControlBar

--- src/components/UpperControlBar/UpperControlBar.elements.tsx
+++ src/components/UpperControlBar/UpperControlBar.elements.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-const UpperControlBarContainer = styled.div`
+export const UpperControlBarContainer = styled.div`
     display: flex;
     justify-content: space-betwween
 `
