acd28481a2449fc05001575d1c805d3fedde7c49
Nahuel1705 <nahuelmoreno1705@gmail.com>
2022-06-27 22:16:51 -0300

    Implementing fonts

--- src/components/SuperTable/SuperTable.tsx
+++ src/components/SuperTable/SuperTable.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import {useSuperTable} from '../../hooks/useSuperTable';
-import {PaginationOptionsEnum} from '../../types/Paginations.types';
-import {SuperTableProps} from '../../types/SuperTable.types';
-import {Pagination} from '../Pagination/Pagination';
-import { UpperControlBar } from '../UpperControlBar/UperControlBar';
-import { Table, Headers, Header, Row} from './SuperTable.elements';
+import React from "react";
+import { useSuperTable } from "../../hooks/useSuperTable";
+import { FontStyle } from "../../styles/FontStyles";
+import { PaginationOptionsEnum } from "../../types/Paginations.types";
+import { SuperTableProps } from "../../types/SuperTable.types";
+import { Pagination } from "../Pagination/Pagination";
+import { UpperControlBar } from "../UpperControlBar/UperControlBar";
+import {
+  Table,
+  Headers,
+  Header,
+  Row,
+  SuperTableContainer,
+} from "./SuperTable.elements";
 
 export const SuperTable = <T extends Object>(props: SuperTableProps<T>) => {
-  const {rows = [], columns} = props;
-  const defaultTextAlign = props.textAling || 'left';
+  const { rows = [], columns } = props;
+  const defaultTextAlign = props.textAling || "left";
 
   const {
     pages,
@@ -19,58 +26,71 @@ export const SuperTable = <T extends Object>(props: SuperTableProps<T>) => {
     handleSort,
     handleSearchInputChange,
     handlePagination,
-  } = useSuperTable<T>(rows, columns, props.defaultSortMethod, props.defaultSearchMethod);
-  console.log('SuperTable Render');
+  } = useSuperTable<T>(
+    rows,
+    columns,
+    props.defaultSortMethod,
+    props.defaultSearchMethod
+  );
+  console.log("SuperTable Render");
 
   return (
     <>
-      <UpperControlBar
-        title={props.title}
-        rowsAmount={rowsAmount}
-        rowLength={rows.length}
-        showSearcher={props.searcher}
-        handleRowsAmmountToShow={handleRowsAmmountToShow}
-        handleSearchInputChange={handleSearchInputChange}
-      />
-      <div>
-        <Table>
-          <Headers>
-            <Row>
-              {normalizedColumns.map((column: any, columnIndex: any) => (
-                <Header
-                  key={columnIndex}
-                  align={column.textAlign || defaultTextAlign}
-                  onClick={() => handleSort(column)}
-                >
-                  {column.title}
-                </Header>
-              ))}
-            </Row>
-          </Headers>
-          <tbody>
-            {currentRows.map((row, rowIndex) => (
-              <tr key={rowIndex}>
+      <FontStyle />
+      <SuperTableContainer>
+        <UpperControlBar
+          title={props.title}
+          rowsAmount={rowsAmount}
+          rowLength={rows.length}
+          showSearcher={props.searcher}
+          handleRowsAmmountToShow={handleRowsAmmountToShow}
+          handleSearchInputChange={handleSearchInputChange}
+        />
+        <div>
+          <Table>
+            <Headers>
+              <Row>
                 {normalizedColumns.map((column: any, columnIndex: any) => (
-                  <td key={columnIndex} align={column.textAlign || defaultTextAlign}>
-                    {column.selector(row)}
-                  </td>
+                  <Header
+                    key={columnIndex}
+                    align={column.textAlign || defaultTextAlign}
+                    onClick={() => handleSort(column)}
+                  >
+                    {column.title}
+                  </Header>
                 ))}
-              </tr>
-            ))}
-          </tbody>
-          {/* <tfoot></tfoot> */}
-        </Table>
-        <div className="bottom-controls">
-          {currentRows.length <= 0 && <p>There's no data to show</p>}
-          {props.pagination && (
-            <Pagination
-              controlOption={props.paginationOption || PaginationOptionsEnum.simple}
-              pages={pages}
-              setCurrentPage={handlePagination}
-            />
-          )}
+              </Row>
+            </Headers>
+            <tbody>
+              {currentRows.map((row, rowIndex) => (
+                <tr key={rowIndex}>
+                  {normalizedColumns.map((column: any, columnIndex: any) => (
+                    <td
+                      key={columnIndex}
+                      align={column.textAlign || defaultTextAlign}
+                    >
+                      {column.selector(row)}
+                    </td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+            {/* <tfoot></tfoot> */}
+          </Table>
+          <div className="bottom-controls">
+            {currentRows.length <= 0 && <p>There's no data to show</p>}
+            {props.pagination && (
+              <Pagination
+                controlOption={
+                  props.paginationOption || PaginationOptionsEnum.simple
+                }
+                pages={pages}
+                setCurrentPage={handlePagination}
+              />
+            )}
+          </div>
         </div>
-      </div>
+      </SuperTableContainer>
     </>
   );
 };
