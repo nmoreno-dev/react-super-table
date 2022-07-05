2573bb4a19e380687362f1fc365de86d4d9c71d0
Nahuel1705 <nahuelmoreno1705@gmail.com>
2022-07-04 22:34:33 -0300

    removed unused import

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from "react";
+import React from "react";
 import { ButtonProps } from "../../types/Button.types";
 import { StyledButton } from "./Button.elements";
 
